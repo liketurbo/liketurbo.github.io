a9f85a93b5542a49fc3c6a6c429aeda78f76d4ec
Ramazan Chasygov <theliketurbo@gmail.com>
2021-06-11 16:48:21 +0300

    Updates

---
+++ _next/static/sMHQbSDzAeUAsfMDsERsY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-8e51f87aa89e52931607.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-06d1338e7da7e40c3998.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
