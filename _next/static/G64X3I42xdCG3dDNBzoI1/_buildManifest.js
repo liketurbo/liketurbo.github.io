719af1f8a621c0a78d5b4c79c4692032368cc3db
liketurbo <liketurbo@users.noreply.github.com>
2022-06-25 18:54:12 +0000

    deploy: f9c9bcf39fd50efd4a161209597899963b5076ed

--- _next/static/G64X3I42xdCG3dDNBzoI1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-e985d6253c241c4a97d7.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-02d4aafe93386926ab02.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
